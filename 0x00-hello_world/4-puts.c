0fd13d35a007fff527a980db468ad9a71fcf2ede
iantovile <iantovile@gmail.com>
2022-04-08 07:08:31 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 /**
  * prints the words as it is
  * C programming language
- * returns 0 as teh value
+ * returns 0 as the value
  */
 
 int main(void)
-
 {
-
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
