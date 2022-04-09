abfce85305a5fcfbb051e6224b435d89f8b87809
iantovile <iantovile@gmail.com>
2022-04-09 08:03:54 -0700

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /*
  * Main - runs the code for printing
  *
- * Return - successful execution will return the int 0
+ * Return - successful execution will return the int "0"
  */
 int main (void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
