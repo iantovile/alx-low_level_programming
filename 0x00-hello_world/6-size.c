75ed9311d8d5ff4772c8f3a8271b26383b25cfa6
iantovile <iantovile@gmail.com>
2022-04-09 08:19:37 -0700

    k

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * Main - primarily runs the code
+ *
+ * Return - a success is seen with the return of 0
+ */
+int main(void)
+{
+	int a;
+	char b;
+	float c;
+	long int d;
+	long long int e;
+
+	printf("Size of a char: %d byte (s)\n", sizeof(b));
+	printf("Size of an int: %d byte(s)\n", sizeof(a));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(d));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(e));
+	printf("Size of a float: %d byte(s)\n", sizeof(c));
+	return (0);
+
+}
