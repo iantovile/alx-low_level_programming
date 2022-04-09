313e53172f3a2f97e29b1f132bb953eca796d3b8
iantovile <iantovile@gmail.com>
2022-04-09 07:42:54 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,11 @@
  * main - we use "puts" to print the output of a string
  *
  * short description - puts will print the senternce in to teh standard output
- *
  * return - the program will return '0' if executed with no errors
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return 0;
 }
