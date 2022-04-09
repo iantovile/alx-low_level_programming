a9b2ed36a1e6ed358f1109126e4f672868feecaa
iantovile <iantovile@gmail.com>
2022-04-09 08:01:34 -0700

    k

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main - runs the code for printing
+ *
+ * Return - successful execution will return the int 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
