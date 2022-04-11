c0af2a740ec74412fbca9243c0ec7df3d3969089
iantovile <iantovile@gmail.com>
2022-04-11 03:46:55 -0700

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/*
+ * Main - printing teh alphabet
+ *
+ * Return - completed with zero
+ */
+
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+	{
+		putchar(abc);
+		abc++;
+	}
+	putchar("\n");
+	return (0);
+
+}
