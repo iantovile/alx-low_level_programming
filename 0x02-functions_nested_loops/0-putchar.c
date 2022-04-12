30168a0f93339339cf235ab2dd0ac52c2817a040
iantovile <iantovile@gmail.com>
2022-04-12 06:46:35 -0700

    my first attempt

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/*
+ * Main - prints "_putchar"
+ *
+ * Returns the value zero always
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This project is about nested loops.
+we use various loops such as wile and for loops to do various things.
