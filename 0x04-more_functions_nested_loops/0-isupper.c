25ba9ca48f4a8a313b752f6eca4dd5074d1a7c9a
iantovile <iantovile@gmail.com>
2022-04-14 17:05:05 -0700

    third time is a charm

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
-#include "maiin.h"
-
+#include "main.h"
 /**
- * _isupper - function that checks for uppercase character.
- *
- * @c: uppercase character.
- *
- * Return: 1 if c is uppercase, 0 otherwise.
-*/
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
+	if (c >= 'A' && c <= 'Z'){
 		return (1);
 	}
-	else
+	else{
 		return (0);
+	}
+
 }
