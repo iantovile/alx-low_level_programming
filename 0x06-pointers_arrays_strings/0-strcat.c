9ef24f8a6befd463788037007c19977d8ac58bf7
iantovile <iantovile@gmail.com>
2022-04-24 23:49:13 +0000

    new

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates strings
+ * @dest: pointer to the changed character
+ * @src: also  pointer to changed character
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
