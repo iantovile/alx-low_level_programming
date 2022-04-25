32d12def4c3135cf97c4b6db753ae5d94d1e89d8
iantovile <iantovile@gmail.com>
2022-04-25 00:30:45 +0000

    betty changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int a, b;
+	int a, b;
 
-        a = 0;
-        while (dest[a] != '\0')
-        {
-                a++;
-        }
+	a = 0;
 
-        b = 0;
-        while (src[b] != '\0')
-        {
-                dest[a] = src[b];
-                b++;
-                a++;
-        }
-
-        dest[a] = '\0';
-
-        return (dest);
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
 }
