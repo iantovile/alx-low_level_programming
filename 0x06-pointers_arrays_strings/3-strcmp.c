007e7ab644ea3a4f0ab803ed7a5674fe7f7e5b84
iantovile <iantovile@gmail.com>
2022-04-25 01:37:08 +0000

    k

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,20 @@ include "main.h"
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
-	int x;
-
-	for ( x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		if (s1[x] != s2[x])
-		{
-			return (s1[x] - s2[x]);
-		}
+		s1++;
+		s2++;
 	}
-	return 0;
 
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
